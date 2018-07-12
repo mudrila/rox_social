a86f972e6dbb51c65b172d1d7025448634a49149
Kirill <klimenkomud@gmail.com>
2018-07-12 13:27:13 +0300

    Update registration styles

--- app/public/components/Pages/UnauthorizedUserPages/Registration/RegistrationPage.jsx
+++ app/public/components/Pages/UnauthorizedUserPages/Registration/RegistrationPage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 import './main.scss'
 export default class RegistrationPage extends Component {
   constructor (props) {
@@ -54,23 +55,47 @@ export default class RegistrationPage extends Component {
     let { userName, userEmail, userPassword, userConfirmPassword } = this.state.formValues;
     return (
       <form name='registration-form' className='b-registration-form'>
-        <h1 className='b-registration-form__header'>Register to Social Rox</h1>
-        <section className='b-registration-form__form-group form-group'>
-          <label className='b-registration-form__form-group__label_required' htmlFor='user-name'>Your name</label>
-          <input required value={userName} name='userName' id='user-name' className='b-registration-form__form-group__input_required' placeholder='Type your name' onChange={this.handleInputChange}/>
-        </section>
-        <section className='b-registration-form__form-group form-group'>
-          <label className='b-registration-form__form-group__label_required' htmlFor='user-email'>Your email</label>
-          <input required type='email' value={userEmail} name='userEmail' id='user-email' className='b-registration-form__form-group__input_required' placeholder='Type your email' onChange={this.handleInputChange}/>
-        </section>
-        <section className='b-registration-form__form-group form-group'>
-          <label className='b-registration-form__form-group__label_required' htmlFor='user-password'>Your password</label>
-          <input required type='password' value={userPassword} name='userPassword' id='user-password' className='b-registration-form__form-group__input_required' placeholder='Type your password' onChange={this.handleInputChange}/>
-        </section>
-        <section className='b-registration-form__form-group form-group'>
-          <label className='b-registration-form__form-group__label_required' htmlFor='user-confirm-password'>Confirm password</label>
-          <input required type='password' value={userConfirmPassword} name='userConfirmPassword' id='user-confirm-password' className='b-registration-form__form-group__input_required' placeholder='Same password again' onChange={this.handleInputChange}/>
-        </section>
+        <h3 className='b-registration-form__header'>Register to Social Rox</h3>
+        <TextField
+          required
+          label='Your name'
+          value={userName}
+          name='userName'
+          id={'user-name'}
+          className='b-registration-form__form-group'
+          onChange={this.handleInputChange}
+        />
+        <TextField
+          required
+          onChange={this.handleInputChange}
+          className='b-registration-form__form-group'
+          value={userEmail}
+          type='email'
+          id={'user-email'}
+          name={'userEmail'}
+          label={'Your email'}
+        />
+        <TextField
+          required
+          onChange={this.handleInputChange}
+          className='b-registration-form__form-group'
+          value={userPassword}
+          type={'password'}
+          id={'user-password'}
+          name={'userPassword'}
+          label={'Your password'}
+        />
+        <TextField
+          required
+          onChange={this.handleInputChange}
+          className='b-registration-form__form-group'
+          value={userConfirmPassword}
+          type={'password'}
+          id={'user-confirm-password'}
+          name={'userConfirmPassword'}
+          label={'Confirm'}
+          placeholder={'Same password again'}
+        />
         <section className='b-registration-form__form-group form-group b-form-control'>
           <Button color='primary' variant='outlined' onClick={this.handleFormSubmit} disabled={!this.state.formValid}>Submit</Button>
         </section>
